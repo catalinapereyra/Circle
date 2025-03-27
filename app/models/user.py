1556611597a28780180bf5768f3152180da5e92b
Rocio Bringas <rocio@MacBook-Air-3.local>
2025-03-27 15:59:37 -0300

    termiando para sign up

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,7 @@
-from app import db  # Importa la instancia de db desde la aplicación (desde el init de la app)
+from . import db  # importar 'db' desde __init__.py en models
 from enum import Enum
 
-class Genders(Enum):
+class Genders(Enum): #clase para generar las opciones de gender
     MALE = 1
     FEMALE = 2
     OTHER = 3
@@ -10,12 +10,12 @@ class User(db.Model):
     __tablename__ = 'user' #nombro a la tabla
 
     #defino todas las columnas de la tabla
-    username = db.Column(db.String(255), unique=True, nullable=False, primary_key=True) #username de tipo string de lomg 255 caracteres
-    password = db.Column(db.String(255), nullable=False) # en SQLAlchemy no hace flata usar varchar, xq string = varchar
-    name = db.Column(db.String(255), nullable=False)
+    username = db.Column(db.String(25), unique=True, nullable=False, primary_key=True) #username de tipo string de lomg 255 caracteres
+    password = db.Column(db.String(30), nullable=False) # en SQLAlchemy no hace flata usar varchar, xq string = varchar
+    name = db.Column(db.String(15), nullable=False)
     age = db.Column(db.Integer, nullable=False)
-    email = db.Column(db.String(255), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
     gender = db.Column(db.Enum(Genders), nullable=False)
     location = db.Column(db.String(255), nullable=False) # ver si hay que hacerlo con coordenadas o que!!!!!!
-    id_subscription = db.Column(db.Integer, db.ForeignKey('subscription.id'), nullable=False)
+    id_subscription = db.Column(db.Integer, db.ForeignKey('subscription.id'), nullable=False) # columna de clave foránea en la tabla actual que hace referencia a la columna id de la tabla subscription
 
