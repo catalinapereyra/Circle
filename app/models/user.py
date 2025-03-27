b57109dbb60511b08ac109ad88c45c7ba0bf0979
Rocio Bringas <rocio@MacBook-Air-3.local>
2025-03-27 15:41:40 -0300

    created table users. ocation was put but have to decide its type

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,21 @@
-from app import circle
-from
+from app import db  # Importa la instancia de db desde la aplicación (desde el init de la app)
+from enum import Enum
 
-class User():
-    __tablename__ = 'users'
+class Genders(Enum):
+    MALE = 1
+    FEMALE = 2
+    OTHER = 3
 
-    username = db.column()
+class User(db.Model):
+    __tablename__ = 'user' #nombro a la tabla
+
+    #defino todas las columnas de la tabla
+    username = db.Column(db.String(255), unique=True, nullable=False, primary_key=True) #username de tipo string de lomg 255 caracteres
+    password = db.Column(db.String(255), nullable=False) # en SQLAlchemy no hace flata usar varchar, xq string = varchar
+    name = db.Column(db.String(255), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    gender = db.Column(db.Enum(Genders), nullable=False)
+    location = db.Column(db.String(255), nullable=False) # ver si hay que hacerlo con coordenadas o que!!!!!!
+    id_subscription = db.Column(db.Integer, db.ForeignKey('subscription.id'), nullable=False)
+
