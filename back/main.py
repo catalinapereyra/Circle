acccc2c5ff02c175078554c96d0473cce19ca908
Rocio Bringas <rocio@MacBook-Air-3.local>
2025-07-13 16:44:21 -0300

    .

--- back/main.py
+++ back/main.py
@@ -34,28 +34,49 @@ def create_app():
          methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"]
          )
 
-    sdk = mercadopago.SDK("TEST-7284458844008139-071315-4a0f0f3e790c3ddf821c59608052c68e-302805625")
+    sdk = mercadopago.SDK("TEST-4448250949116850-071315-308b9b75d0b0af062d1120459069e3a5-302805625")
 
-    @app.route("/make_payment", methods=["POST"])
-    def make_payment():
-        preference_data = {
-            "items": [
-                {
-                    "title": "Circle Premium subs",
-                    "quantity": 1,
-                    "unit_price": 999,
-                    "currency_id": "ARS"
-                }
-            ]
+    # @app.route("/make_payment", methods=["POST"])
+    # def make_payment():
+    #     preference_data = {
+    #         "items": [
+    #             {
+    #                 "title": "Circle Premium subs",
+    #                 "quantity": 1,
+    #                 "unit_price": 999,
+    #                 "currency_id": "ARS"
+    #             }
+    #         ]
+    #     }
+    #
+    #     preference_response = sdk.preference().create(preference_data)
+    #     print(preference_response)
+    #     preference = preference_response.get("response", {})
+    #     print("✅ URL del pago:", preference["init_point"])
+    #     return jsonify({
+    #         "preference_id": preference["id"],
+    #         "sandbox_init_point": preference["sandbox_init_point"]
+    #     })
+
+    @app.route("/crear_suscripcion", methods=["POST"])
+    def crear_suscripcion():
+        preapproval_data = {
+            "reason": "Circle Premium Subscription",
+            "auto_recurring": {
+                "frequency": 1,
+                "frequency_type": "months",
+                "transaction_amount": 999,
+                "currency_id": "ARS",
+                "start_date": "2025-07-13T00:00:00.000-03:00",
+                "end_date": "2026-07-13T00:00:00.000-03:00"
+            },
+            "back_url": "http://localhost:5173/choose-profile",
+            "payer_email": "mail@de.prueba.com",  
         }
 
-        preference_response = sdk.preference().create(preference_data)
-        print(preference_response)
-        preference = preference_response.get("response", {})
-        print("✅ URL del pago:", preference["init_point"])
+        result = sdk.preapproval().create(preapproval_data)
         return jsonify({
-            "preference_id": preference["id"],
-            "sandbox_init_point": preference["sandbox_init_point"]
+            "init_point": result["response"]["init_point"]
         })
 
     @app.before_request
